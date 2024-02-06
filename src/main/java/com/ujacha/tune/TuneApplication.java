0fd6fcfa1a87afe8a447f38c7cf63605e387f614
KimRPG <hunsung9909@gmail.com>
2024-02-06 14:29:36 +0900

    init : tune 프로젝트 시작

---
+++ src/main/java/com/ujacha/tune/TuneApplication.java
@@ -0,0 +1,13 @@
+package com.ujacha.tune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujacha/tune/TuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujacha.tune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
