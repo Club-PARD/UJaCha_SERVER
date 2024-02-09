5568dc11cd28d934b012b0df4db88aae220716ae
KimRPG <hunsung9909@gmail.com>
2024-02-09 09:37:32 +0900

    feat : ExceptionHandler 추가

---
+++ src/main/java/com/ujacha/tune/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ujacha.tune.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("NullPointerException occurred: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("IllegalStateException occurred: " + ex.getMessage());
+
+}
