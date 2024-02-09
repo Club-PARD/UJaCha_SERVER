595c34081968bdab79b32ee24ba749f07a8ab96b
KimRPG <hunsung9909@gmail.com>
2024-02-09 09:37:14 +0900

    feat : security config

--- src/main/java/com/ujacha/tune/auth/config/SecurityConfig.java
+++ src/main/java/com/ujacha/tune/auth/config/SecurityConfig.java
@@ -4,19 +4,24 @@ import com.ujacha.tune.auth.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.nio.file.AccessDeniedException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -27,13 +32,13 @@ public class SecurityConfig {
                 //basic 인증 (현재는 bearer token 인증방법을 사용하기 때문에 비활성화)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 // 세션 기반 인증 (현재는 Session 기반 인증을 사용하지 않기 때문에 상태를 없앰)
-                .sessionManagement((sessionManagement) ->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // "/" "/api/auth" 모듈에 대해서는 모두 허용 (인증을 하지 않고 사용 가능하게 함)
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests.requestMatchers("/", "/api/member/**")
                                 .permitAll()
-                        //나머지 REquest에 대해서는 모두 인증된 사용자만 사용가능하게함
-                        .anyRequest().authenticated());
+                                //나머지 REquest에 대해서는 모두 인증된 사용자만 사용가능하게함
+                                .anyRequest().authenticated());
 
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
