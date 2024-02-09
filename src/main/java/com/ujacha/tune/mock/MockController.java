6d338261d619c7a7c1120ac363723f29adcc3025
KimRPG <hunsung9909@gmail.com>
2024-02-09 16:02:43 +0900

    mock 파일 생성

---
+++ src/main/java/com/ujacha/tune/mock/MockController.java
@@ -0,0 +1,22 @@
+package com.ujacha.tune.mock;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mock")
+public class MockController {
+    @GetMapping("/test")
+    public ResponseDTO test() {
+        return new ResponseDTO(1,
+                2,3,
+                4,5,
+                15,LocalDate.now());
+    }
+}
