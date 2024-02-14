2d2b7d4827665a3626f8eb6fede8636c76d81042
KimRPG <hunsung9909@gmail.com>
2024-02-14 11:05:33 +0900

    feat : Cors 오류 해결

---
+++ src/main/java/com/ujacha/tune/member/controller/CorsController.java
@@ -0,0 +1,20 @@
+package com.ujacha.tune.member.controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RestController
+@RequestMapping("/")
+public class CorsController {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns();
+            }
+        };
+    }
+}
